85eb94a6985ed503036e1b9d4a449f76abce66d7
randhid <rand@viam.com>
2024-03-08 13:51:23 -0500

    Move registration to main

--- src/main.py
+++ src/main.py
@@ -7,8 +7,14 @@ import asyncio
 from viam.components.arm import Arm
 from viam.components.gripper import Gripper
 from viam.module.module import Module
+
+from viam.components.arm import Arm
+from viam.components.gripper import Gripper
+from viam.resource.registry import Registry, ResourceCreatorRegistration
+
 from .yahboom import yahboom
 from .yahboom_gripper import yahboom_gripper
+
 
 async def main():
     """This function creates and starts a new module, after adding all desired resources.
@@ -16,6 +22,18 @@ async def main():
     Args:
         address (str): The address to serve the module on
     """
+
+    Registry.register_resource_creator(
+    Arm.SUBTYPE, yahboom.MODEL,
+    ResourceCreatorRegistration(yahboom.new, yahboom.validate)
+    )
+    
+    Registry.register_resource_creator(
+    Gripper.SUBTYPE,
+    yahboom_gripper.MODEL,
+    ResourceCreatorRegistration(yahboom_gripper.new, yahboom_gripper.validate
+    ))
+
     module = Module.from_args()
     module.add_model_from_registry(Arm.SUBTYPE, yahboom.MODEL)
     module.add_model_from_registry(Gripper.SUBTYPE, yahboom_gripper.MODEL)
